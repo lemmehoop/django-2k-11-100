a5cd00f792fd51d6bc254ae83e62ba53720677cf
atnartur <i@atnartur.ru>
2022-09-16 18:50:37 +0300

    + redirect

--- web/urls.py
+++ web/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 
-from web.views import main_view
+from web.views import notes_view, main_view
 
 urlpatterns = [
     path("", main_view),
+    path("notes/", notes_view, name="notes_list"),
 ]
