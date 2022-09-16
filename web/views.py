f0eb3a181b54c77c9af827d0d040f746ebe791ea
atnartur <i@atnartur.ru>
2022-09-16 17:13:01 +0300

    + alerts filter

--- web/views.py
+++ web/views.py
@@ -7,6 +7,10 @@ from web.models import Note
 def main_view(request):
     with_alerts = 'with_alerts' in request.GET
     notes = Note.objects.all()
+
+    if with_alerts:
+        notes = notes.filter(alert_send_at__isnull=False)
+
     return render(request, "web/main.html", {
         'count': Note.objects.count(),
         'notes': notes,
