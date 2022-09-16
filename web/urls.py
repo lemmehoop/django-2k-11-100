13c287cadaaa6734d350253a5973dd79e88a110c
atnartur <i@atnartur.ru>
2022-09-16 18:55:58 +0300

    + note page

--- web/urls.py
+++ web/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.urls import path
 
-from web.views import notes_view, main_view
+from web.views import notes_view, main_view, note_view
 
 urlpatterns = [
     path("", main_view),
     path("notes/", notes_view, name="notes_list"),
+    path("notes/<int:id>/", note_view, name="note"),
 ]
