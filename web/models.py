33c087e634dd6766ddd1443125944c25bd3a25fd
atnartur <i@atnartur.ru>
2022-09-16 16:33:05 +0300

    + model to string

--- web/models.py
+++ web/models.py
@@ -1,30 +1,33 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
 
 class BaseModel(models.Model):
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
+
 
 class Tag(BaseModel):
-	title = models.CharField(max_length=200)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	parent_tag = models.ForeignKey(
-		"self",
-		on_delete=models.SET_NULL,
-		null=True
-	)
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_tag = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True
+    )
 
 
 class Note(BaseModel):
-	title = models.CharField(max_length=500)
-	text = models.TextField()
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	alert_send_at = models.DateTimeField(null=True)
-	tags = models.ManyToManyField(Tag)
+    title = models.CharField(max_length=500)
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    alert_send_at = models.DateTimeField(null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f'Note #{self.id} "{self.title}"'
