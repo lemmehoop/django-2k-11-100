5d73272a4cc48e6294bece07e2f5fbb3fd3ac6a4
atnartur <i@atnartur.ru>
2022-09-16 17:08:39 +0300

    + with_alerts get param

--- web/views.py
+++ web/views.py
@@ -5,23 +5,10 @@ from web.models import Note
 
 
 def main_view(request):
+    with_alerts = 'with_alerts' in request.GET
     notes = Note.objects.all()
-
-    # for note in notes:
-    #     print(note)
-    #     print(note.title)
-    #     print(note.text)
-    #     print(note.user)
-    #     print(note.user.first_name)
-    #
-    #     # SELECT auth_user.id FROM web_note
-    #     # INNER JOIN auth_user ON web_note.user_id = auth_user.id
-    #     print(note.user.id)
-    #
-    #     # SELECT user_id FROM web_note
-    #     print(note.user_id)
-
     return render(request, "web/main.html", {
         'count': Note.objects.count(),
-        'notes': notes
+        'notes': notes,
+        'with_alerts': with_alerts
     })
