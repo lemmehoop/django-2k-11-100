a01590cc73e98800f8928149659899934c8d651b
atnartur <i@atnartur.ru>
2022-11-11 19:16:19 +0400

    + env file in settings

--- notes/settings.py
+++ notes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-28jr4_2q50+t+286gz847=)oi5^7ki^z1q61k%49v6#a7#b^_&"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-28jr4_2q50+t+286gz847=)oi5^7ki^z1q61k%49v6#a7#b^_&")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "true").lower() == "true"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*'] if DEBUG else ['test.ru']
 
 
 # Application definition
@@ -79,11 +79,11 @@ WSGI_APPLICATION = "notes.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "notes",
-        "USER": "notes",
-        "PASSWORD": "notes",
-        "HOST": "localhost",
-        "PORT": 5432
+        "NAME": os.environ.get("DB_NAME", "notes"),
+        "USER": os.environ.get("DB_USER", "notes"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "notes"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": int(os.environ.get("DB_PORT", 5432))
     }
 }
 
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
