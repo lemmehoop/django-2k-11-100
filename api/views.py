aa9532fcb05868c27c357e06a9658b5b65eba9f5
atnartur <i@atnartur.ru>
2022-11-18 19:45:27 +0400

    + model viewset

--- api/views.py
+++ api/views.py
@@ -1,9 +1,7 @@
 from django.db.models import Prefetch
-from rest_framework import status, mixins
 from rest_framework.decorators import api_view
-from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from api.serializers import NoteSerializer
 from web.models import Note, NoteComment
@@ -14,43 +12,8 @@ def status_view(request):
     return Response({"status": "ok"})
 
 
-class NoteViewSet(
-    mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, GenericViewSet
-):
+class NoteViewSet(ModelViewSet):
     queryset = Note.objects.all().optimize_for_lists().prefetch_related(
         Prefetch('comments', NoteComment.objects.all().order_by("created_at"))
     )
     serializer_class = NoteSerializer
-
-
-# @api_view(['GET', 'POST'])
-# def notes_view(request):
-#     if request.method == 'POST':
-#         serializer = NoteSerializer(
-#             data=request.data,
-#             context={"request": request}
-#         )
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     notes = Note.objects.all().optimize_for_lists().prefetch_related(
-#         Prefetch('comments', NoteComment.objects.all().order_by("created_at"))
-#     )
-#     serializer = NoteSerializer(notes, many=True)
-#     return Response(serializer.data)
-
-
-# @api_view(['GET', 'PUT'])
-# def note_view(request, id):
-#     note = get_object_or_404(Note, id=id)
-#     if request.method == "PUT":
-#         serializer = NoteSerializer(
-#             instance=note,
-#             data=request.data,
-#             context={"request": request}
-#         )
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-#     serializer = NoteSerializer(note)
-#     return Response(serializer.data)
