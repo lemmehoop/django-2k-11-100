f28912f31149ae3f6718200df9be2b4a18791c17
atnartur <i@atnartur.ru>
2022-11-18 19:01:52 +0400

    + create note

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Prefetch
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
@@ -12,8 +13,18 @@ def status_view(request):
     return Response({"status": "ok"})
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def notes_view(request):
+    if request.method == 'POST':
+        data = request.data
+        serializer = NoteSerializer(
+            data=data,
+            context={"request": request},
+            initial={"user_id": request.user.id}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     notes = Note.objects.all().optimize_for_lists().prefetch_related(
         Prefetch('comments', NoteComment.objects.all().order_by("created_at"))
     )
