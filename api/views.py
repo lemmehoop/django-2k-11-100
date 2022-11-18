101c8c51d21818c56c60d406743629d1c2cece66
atnartur <i@atnartur.ru>
2022-11-18 18:35:53 +0400

    + note serializer

--- api/views.py
+++ api/views.py
@@ -1,7 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from api.serializers import NoteSerializer
+from web.models import Note
+
 
 @api_view()
 def status_view(request):
     return Response({"status": "ok"})
+
+
+@api_view()
+def notes_view(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
